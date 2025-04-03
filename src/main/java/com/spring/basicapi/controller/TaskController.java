f602d2532b99ce273ca767b2abe9f75c2eff7fa7
Gabriel <gabrielglira220@gmail.com>
2025-04-03 11:35:51 -0300

    update

--- src/main/java/com/spring/basicapi/controller/TaskController.java
+++ src/main/java/com/spring/basicapi/controller/TaskController.java
@@ -1,18 +1,14 @@
 package com.spring.basicapi.controller;
-
-
 import com.spring.basicapi.dto.TaskDTO;
 import com.spring.basicapi.model.Task;
 import com.spring.basicapi.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -26,5 +22,16 @@ public class TaskController {
     public Task createTask(@RequestBody TaskDTO taskDTO){
         return Optional.ofNullable(taskService.createTask(taskDTO))
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "ERRO WHEN CREATE TASK!"));
+    }
+
+    @GetMapping("/list")
+    public List<Task> getTasks(){
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/")
+    public Task getTaskById(@RequestBody Long id){
+        return Optional.ofNullable(taskService.getTaskById(id))
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "ERRO WHEN GET TASK"));
     }
 }
--- src/main/java/com/spring/basicapi/service/TaskService.java
+++ src/main/java/com/spring/basicapi/service/TaskService.java
@@ -8,6 +8,9 @@ import com.spring.basicapi.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class TaskService {
     @Autowired
@@ -27,4 +30,11 @@ public class TaskService {
 
     }
 
+    public List<Task> getAllTasks(){ return taskRepo.findAll();
+    }
+
+    public Task getTaskById(Long id) {
+        Optional<Task> task = taskRepo.findById(id);
+        return task.orElse(null);
+    }
 }
