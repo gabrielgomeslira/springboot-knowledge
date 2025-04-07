84e3476588b59597469f20eb66d36c1f01869898
Gabriel Gomes <gabrielglira220@gmail.com>
2025-04-07 14:19:20 -0300

    update

--- src/main/java/com/spring/basicapi/service/UserService.java
+++ src/main/java/com/spring/basicapi/service/UserService.java
@@ -4,7 +4,6 @@ import com.spring.basicapi.dto.LoginResponse;
 import com.spring.basicapi.dto.UserDTO;
 import com.spring.basicapi.model.User;
 import com.spring.basicapi.repository.UserRepo;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
